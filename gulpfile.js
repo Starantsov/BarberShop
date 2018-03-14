e91dc5f5a0c65967ae61ab19fac5ce3f3caaa0c5
Starantsov <nazarstarantsov@gmail.com>
2018-03-14 22:27:29 +0300

    das

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,73 @@
 var gulp = require("gulp");       //Подключения самого 'gulp'
-var less = require("gulp-less"); //Подключение плагина 'gulp-less'
-var browser_sync = require('browser-sync');
-var reload = browser_sync.reload;  //full reload of the page
+var less = require('gulp-less');
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 var del = require("del");
 
 
-gulp.task("default",function(){
-        console.log("Try 'run' that runs local server on your Wi-Fi and turns on monitoring of all changes.");
-});
+var     vf            = require('vinyl-file'),
+        vss           = require('vinyl-source-stream'),
+        vb            = require('vinyl-buffer');
 
-gulp.task("run", ['less','build','browser-sync','watch'],function(){
+gulp.task("run", ['server'],function(){
         console.log("Server started,all your changings are monitoring");
 });
+
 gulp.task("clean",function(){
         return del.sync('build');
 });
 
-gulp.task("build",["clean" ,"less"],function(){
-         
-        var build_html =
-         gulp.src("src/index.html")
-        .pipe(gulp.dest("build"))
-
-        var build_css =
-        gulp.src("src/css/style.css")
-        .pipe(gulp.dest("build/css"))//Отправка в папку готового проекта
-         
-        var build_img =
-        gulp.src("src/img/**/*")
-        .pipe(gulp.dest("build/img"))
-
-});
-
 gulp.task("less",function(){    //Создания таска
-       gulp.src("src/less/style.less")//Указания источника
-       .pipe(less())              //Создание потока и загрузка плагина 'gulp-less'
-       .pipe(gulp.dest("src/css")) //Отправка в папку с исходниками 
-       .pipe(gulp.dest("build/css"))//Отправка в папку готового проекта
-});
-
-gulp.task("html-refresh",function(){
-        gulp.src("src/index.html")
-        .pipe(gulp.dest("build"))
-});
-
-gulp.task("watch", function(){
-        gulp.watch('src/less/**/*.less',['less',reload]);
-        gulp.watch("src/*.html", ['html-refresh',reload]); 
-        //gulp.watch("src/js/*.js", reload);
-});
-
-gulp.task("browser-sync", function(){
-        browser_sync({
-                server: {
-                        baseDir:"build"
-                },
-               notify : false
-        });
+       gulp.src( "src/less/style.less")//Указания источника
+        .pipe(plumber())
+        .pipe(less())           //Создание потока и загрузка плагина 'gulp-less'
+        .pipe(postcss([
+                autoprefixer({browsers:[
+                        "last 1 version",
+                        "last 2 Chrome versions",
+                        "last 2 Firefox versions",
+                        "last 2 Opera versions",
+                        "last 2 Edge versions"
+                ]})
+        ]))
+        .pipe(gulp.dest( "src/css")) //Отправка в папку с исходниками 
+        .pipe(server.reload({stream:true}));
 });
 
 
 
- 
+
+gulp.task("server", ["less"] ,function(){
+       server.init({
+               server: "./src/",
+               injectChanges: true,            
+               logConnections: true
+               
+       });
+
+       gulp.watch("src/less/**/*.less", ["less"]);
+       gulp.watch("src/*.html").on("change",server.reload);
+});
+
+
+// gulp.task("build",["clean" ,"less"],function(){
+         
+//         var build_html =
+//          gulp.src("src/index.html")
+//         .pipe(gulp.dest("build"))
+//         build_html;
+//         var build_css =
+//         // gulp.src("src/less/style.less")
+//         // .pipe(less())
+//         gulp.src("src/css/style.css")
+//         .pipe(gulp.dest("build/css"))//Отправка в папку готового проекта
+//         build_css; 
+
+//         var build_img =
+//         gulp.src("src/img/**/*")
+//         .pipe(gulp.dest("build/img"))
+//         build_img;
+
+// });
